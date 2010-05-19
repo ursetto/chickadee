64d44b09778c92370183f28d20304dbbb9980a20
Jim <jim@abbey.3e8.org>
2010-05-19 00:58:36 -0700

    use no-repeat property; fixes ipad mag display

--- chickadee-driver-3e8.scm
+++ chickadee-driver-3e8.scm
@@ -12,10 +12,10 @@
 (cdoc-uri (uri "/cdoc"))
 (chickadee-uri (uri "/chickadee"))
 (incremental-search-uri (uri "/cdoc/ajax/prefix"))
-(chickadee-css-files (list (uri "/cdoc/chickadee.css")))
+(chickadee-css-files (list (uri "/cdoc/chickadee.css?2")))
 (chickadee-js-files (list (uri "http://code.jquery.com/jquery-1.4.2.min.js")
                           (uri "/cdoc/jquery.metadata.2.1.min.js")
-                          (uri "/cdoc/chickadee-jquery.js")))
+                          (uri "/cdoc/chickadee-jquery.js?1")))
 
 (maximum-match-results 250)
 (maximum-match-signatures 100)
