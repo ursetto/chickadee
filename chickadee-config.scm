70c9fa4bf85c75c688f226ca0352ea4d4135a60f
Jim Ursetto <zb@3e8.org>
2010-10-22 01:26:49 -0500

    Add chickadee-cmd

--- chickadee-config.scm
+++ chickadee-config.scm
@@ -1,31 +1,30 @@
-(use chickadee spiffy uri-common)
-(define uri uri-reference)
+;;; Chickadee chicken-doc server configuration file
 
-(root-path "./root")
-;(debug-log (current-error-port))
+;; chickadee changes to the directory holding the conf-file on startup,
+;; so all paths below are relative to that path.
+
+(use chickadee spiffy uri-common)   ;; REQUIRED.  Do not remove.
+(define uri uri-reference)          ;; A convenient alias.
+
+;;; Main config
+
+(root-path "root")
 (server-port 8080)
-(access-log "logs/access.log")
-;(access-log (current-output-port))
-
-;; Note: if connections is small and AJAX parallel requests
-;; are allowed, spiffy may take tens of seconds to respond,
-;; occasionally.  May be an issue with keepalives.
+;(access-log "logs/access.log")
+;(error-log "logs/error.log")
+;(debug-log (current-error-port))
+;(ajax-log "logs/ajax.log")         ;; CPU intensive; not recommended for production.
 
 ;; An even number of connections is desirable as user agents
 ;; usually open two (keep-alive) connections per hostname.
 (max-connections 8)
-
-(ajax-log "/tmp/ajax.log")
 (cdoc-uri (uri "/cdoc"))
 (chickadee-uri (uri "/chickadee"))
 (incremental-search-uri (uri "/cdoc/ajax/prefix"))
 (chickadee-css-files (list (uri "/cdoc/chickadee.css")))
 (chickadee-js-files (list (uri "http://code.jquery.com/jquery-1.4.2.min.js")
                           (uri "/cdoc/jquery.metadata.2.1.min.js")
-                          (uri "/cdoc/chickadee-jquery.js")
-;                          (uri "http://ie7-js.googlecode.com/svn/version/2.1%20(beta4)/IE9.js")
-;                          (uri "http://ie7-js.googlecode.com/svn/version/2.1%20(beta4)/ie7-squish.js")
-                          ))
+                          (uri "/cdoc/chickadee-jquery.js")))
 
 (maximum-match-results 250)
 (maximum-match-signatures 100)
--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-DEST=./root/cdoc
+BASE=.
+DEST=$BASE/root/cdoc
 
 mkdir -p $DEST
+mkdir -p $BASE/logs
 ln -f chickadee-jquery.js $DEST
 ln -f chickadee.css $DEST
 ln -f jquery.metadata.min.js $DEST/jquery.metadata.2.1.min.js
