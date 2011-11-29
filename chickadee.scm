4ca94a87408c310a81ea68d6da847e1169661436
Jim Ursetto <jim@3e8.org>
2011-11-28 21:15:26 -0600

    Also add "js" class to HTML if javascript

--- chickadee.scm
+++ chickadee.scm
@@ -321,9 +321,9 @@
      (html (@ class "no-js")
       (head ,(charset "utf-8")
             ,(map css-link (chickadee-css-files))
-            ;; Remove "no-js" class from <HTML> when JS enabled, a la Modernizr.
+            ;; Remove "no-js" class and add "js" class to <HTML> when JS enabled, a la Modernizr.
             (script "this.document.documentElement.className = "
-                    "this.document.documentElement.className.replace(/\\bno-js\\b/, '')")
+                    "this.document.documentElement.className.replace(/\\bno-js\\b/, '') + ' js '")
             (title ,(if page-title
                         `(,page-title " | chickadee")
                         "chickadee server"))
