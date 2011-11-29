cf19a99f41501261e6661b4c43c3bacec74a7b72
Jim Ursetto <zb@3e8.org>
2011-11-28 22:44:04 -0500

    update chickadee-driver-3e8 again (forgot to install.sh)

--- chickadee-driver-3e8.scm
+++ chickadee-driver-3e8.scm
@@ -12,7 +12,7 @@
 (cdoc-uri (uri "/cdoc"))
 (chickadee-uri (uri "/doc"))
 (incremental-search-uri (uri "/cdoc/ajax/prefix"))
-(chickadee-css-files (list (uri "/cdoc/chickadee.css?14")))
+(chickadee-css-files (list (uri "/cdoc/chickadee.css?16")))
 (chickadee-js-files (list (uri "http://code.jquery.com/jquery-1.4.2.min.js")
                           (uri "/cdoc/jquery.metadata.2.1.min.js")
                           (uri "/cdoc/chickadee-jquery.js?5")))
