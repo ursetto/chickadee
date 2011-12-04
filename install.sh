28c1843151872e77fdb328f27099f08da5a3654a
Jim Ursetto <jim@3e8.org>
2011-12-03 23:13:06 -0600

    Add custom modernizr (just csscolumns), update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,8 @@ mkdir -p $BASE/logs
 ln -f chickadee-jquery.js $DEST
 ln -f chickadee.css $DEST
 ln -f jquery.metadata.min.js $DEST/jquery.metadata.2.1.min.js
+for i in modernizr*js; do
+  ln -f "$i" $DEST/
+done
 ln -f mag.png $DEST
 
