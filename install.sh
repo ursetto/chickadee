af2a57ac383f9abfe684b5155628e9ae17887171
Jim Ursetto <jim@3e8.org>
2011-12-13 02:37:40 -0600

    Add (minified) prettify-bundle.js = prettify.js + lang-lisp/css/sql

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ mkdir -p $BASE/logs
 ln -f chickadee-jquery.js $DEST
 ln -f chickadee.css $DEST
 ln -f jquery.metadata.min.js $DEST/jquery.metadata.2.1.min.js
+ln -f prettify-bundle.js $DEST
 for i in modernizr*js; do
   ln -f "$i" $DEST/
 done
