39445bf15a98f973c7c18dc0271edaa43361cf86
Jim Ursetto <jim@3e8.org>
2011-12-13 22:25:14 -0600

    Remove now-unused jquery metadata plugin

--- chickadee-config.scm
+++ chickadee-config.scm
@@ -29,7 +29,6 @@
 (chickadee-css-files (list (uri "/cdoc/chickadee.css")))
 (chickadee-early-js-files (list (uri "/cdoc/modernizr.respond.93248.js")))
 (chickadee-js-files (list (uri "http://code.jquery.com/jquery-1.4.4.min.js")
-                          (uri "/cdoc/jquery.metadata.2.1.min.js")
                           (uri "/cdoc/chickadee-jquery.js")
                           ;(uri "/cdoc/prettify-bundle.js")  ;; Uncomment if using prettify-js highlighter.
                           ))
--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ mkdir -p $DEST
 mkdir -p $BASE/logs
 ln -f chickadee-jquery.js $DEST
 ln -f chickadee.css $DEST
-ln -f jquery.metadata.min.js $DEST/jquery.metadata.2.1.min.js
 ln -f prettify-bundle.js $DEST
 for i in modernizr*js; do
   ln -f "$i" $DEST/
